5cfe3636d05395d2552ebaa33d55ac64c7d5bd93
Giovane Cunha Mocellin <Giovane Cunha Mocellin>
2016-10-29 13:14:09 -0200

    arrumei collisao

--- player/player.gd
+++ player/player.gd
@@ -12,15 +12,22 @@ func _ready():
 	pass
 
 func _fixed_process(delta):
+	var velocity = Vector2()
 	var delta_mov = Vector2()
 	
 	if (Input.is_action_pressed("move_up")):
-    	delta_mov.y += -SPEED * delta
+		delta_mov.y += -SPEED * delta
 	if (Input.is_action_pressed("move_down")):
-    	delta_mov.y += SPEED * delta
+		delta_mov.y += SPEED * delta
 	if (Input.is_action_pressed("move_left")):
-    	delta_mov.x += -SPEED * delta
+		delta_mov.x += -SPEED * delta
 	if (Input.is_action_pressed("move_right")):
-    	delta_mov.x += SPEED * delta
+		delta_mov.x += SPEED * delta
 	
 	move(delta_mov)
+	
+	if (is_colliding()):
+		var n = get_collision_normal()
+		delta_mov = n.slide(delta_mov)
+		velocity = n.slide(velocity)
+		move(delta_mov)
