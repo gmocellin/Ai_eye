23720088eab1f9f17efe64010063d255b7b94319
gilzoide <gilzoide@gmail.com>
2016-10-29 20:04:51 -0200

    FIX nos créditos e história

---
+++ creditos/creditos.gd
@@ -0,0 +1,35 @@
+extends AnimatedSprite
+
+export var tempoSeg = 10
+export var tempoFade = 2
+var fadeIni
+var tempo
+var atual = 0
+
+func _ready():
+	fadeIni = tempoSeg - tempoFade
+	tempo = 0
+	set_process(true)
+	set_process_input(true)
+
+func _process(delta):
+	tempo += delta
+	if tempo < tempoFade:
+		set_opacity(tempo / tempoFade)
+	elif tempo > tempoSeg:
+		tempo = 0
+		atual += 1
+		if atual == 1:
+			set_frame(atual)
+		else:
+			proximooo()
+	elif tempo > fadeIni:
+		set_opacity(1 - (tempo - fadeIni) / tempoFade)
+
+func _input(event):
+	if event.is_pressed() and event.type in [InputEvent.KEY, InputEvent.MOUSE_BUTTON
+			, InputEvent.JOYSTICK_BUTTON, InputEvent.SCREEN_TOUCH]:
+		proximooo()
+
+func proximooo():
+	get_tree().change_scene("res://menu/menu.tscn")
