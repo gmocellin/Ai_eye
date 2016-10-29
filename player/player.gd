38c9417b48d88a1ac6a121ff48f7466edb476d19
Giovane Cunha Mocellin <Giovane Cunha Mocellin>
2016-10-29 10:35:26 -0200

    Colisão arrumada

---
+++ player/player.gd
@@ -0,0 +1,26 @@
+
+extends KinematicBody2D
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+const SPEED = 150
+
+func _ready():
+	set_fixed_process(true)
+	pass
+
+func _fixed_process(delta):
+	var delta_mov = Vector2()
+	
+	if (Input.is_action_pressed("move_up")):
+    	delta_mov.y += -SPEED * delta
+	if (Input.is_action_pressed("move_down")):
+    	delta_mov.y += SPEED * delta
+	if (Input.is_action_pressed("move_left")):
+    	delta_mov.x += -SPEED * delta
+	if (Input.is_action_pressed("move_right")):
+    	delta_mov.x += SPEED * delta
+	
+	move(delta_mov)
